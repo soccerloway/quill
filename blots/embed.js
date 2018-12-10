c9d3060478e66b4a59d23aaba0e497495993484a
loway <lw54760187@hotmail.com>
2018-12-10 13:22:16 +0800

    [update]

--- blots/embed.js
+++ blots/embed.js
@@ -45,26 +45,20 @@ class Embed extends EmbedBlot {
         };
       }
     } else if (node === this.rightGuard) {
-      let startOffset = text.length;
-      if (node.data[node.data.length - 1] === GUARD_TEXT) {
-        // If the guard is on the right of the inserted text we need to
-        // compensate that extra space.
-        startOffset += 1;
-      }
-
       if (this.next instanceof TextBlot) {
+        if (!text.length || text.length < 1) return
         console.log(text)
         this.next.insertAt(0, text);
         range = {
           startNode: this.next.domNode,
-          startOffset: startOffset
+          startOffset: text.length
         };
       } else {
         textNode = document.createTextNode(text);
         this.parent.insertBefore(this.scroll.create(textNode), this.next);
         range = {
           startNode: textNode,
-          startOffset: startOffset
+          startOffset: text.length
         };
       }
     }
